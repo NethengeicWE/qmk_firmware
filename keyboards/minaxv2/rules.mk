3c55b0a6ff9f98b67d7385a37950ba260787cf56
NWE <test@114514.com>
2024-07-31 19:45:08 +0800

    minaxv2: Fix rules.mk,info.json

--- keyboards/minaxv2/rules.mk
+++ keyboards/minaxv2/rules.mk
@@ -1,13 +1,13 @@
 # This file intentionally left blank
 # enable
 POINTING_DEVICE_DRIVER     	= analog_joystick
-HAPTIC_DRIVER 				+= SOLENOID
+HAPTIC_DRIVER 				= solenoid
 SEND_STRING_ENABLE 			= yes
 ANALOG_DRIVER_REQUIRED 		= yes
 LTO_ENABLE					= yes
 KEY_OVERRIDE_ENABLE 		= yes
 OLED_ENABLE 				= yes
-OLED_DRIVER					= SSD1306
+OLED_DRIVER					= ssd1306
 OLED_TANSPORT 				= i2c
 # disable 
 DEBUG_ENABLE 				= no
